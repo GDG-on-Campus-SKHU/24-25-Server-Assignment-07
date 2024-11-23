eb4084d555aed35dc002e212b0fc568438bcb5e9
Kim, Kiwoong <opkiwoong@naver.com>
2024-11-24 05:14:49 +0900

    24-25-Server-Assignment-07_김기웅 과제제출

---
+++ Kiwoong/src/main/java/junwatson/mychat/dto/TokenDto.java
@@ -0,0 +1,11 @@
+package junwatson.mychat.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class TokenDto {
+    private String accessToken;
+    private String refreshToken;
+}
